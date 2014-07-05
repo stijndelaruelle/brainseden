9887706bd62fa8ba7ea589bb8756b1b0e05ae5d6
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2014-07-05 14:46:47 +0100

    Added 2 player input.

--- Assets/Scripts/ShepherdBehaviour.cs
+++ Assets/Scripts/ShepherdBehaviour.cs
@@ -16,10 +16,12 @@ public class ShepherdBehaviour : MonoBehaviour
         //bool held = Input.GetButton("Jump");
         //bool up = Input.GetButtonUp("Jump");
 
-        float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
+        float horizontal = Input.GetAxis("Player1_Horizontal");
+        float vertical = Input.GetAxis("Player1_Vertical");
 
         rigidbody.AddForce(Vector3.right * horizontal * 2000.0f, ForceMode.Acceleration);
         rigidbody.AddForce(Vector3.forward * vertical * 2000.0f, ForceMode.Acceleration);
+
+        transform.LookAt(transform.position + new Vector3(horizontal, 0.0f, vertical));
 	}
 }
