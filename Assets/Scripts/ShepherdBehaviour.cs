a80925e787181fb9a599aefe68be2f4649375cf3
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2014-07-05 14:30:06 +0100

    Added shepherd movement

--- Assets/Scripts/ShepherdBehaviour.cs
+++ Assets/Scripts/ShepherdBehaviour.cs
@@ -12,6 +12,14 @@ public class ShepherdBehaviour : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-	
+	    //bool down = Input.GetButtonDown("Jump");
+        //bool held = Input.GetButton("Jump");
+        //bool up = Input.GetButtonUp("Jump");
+
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        rigidbody.AddForce(Vector3.right * horizontal * 2000.0f, ForceMode.Acceleration);
+        rigidbody.AddForce(Vector3.forward * vertical * 2000.0f, ForceMode.Acceleration);
 	}
 }
