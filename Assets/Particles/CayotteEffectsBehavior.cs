d4bea475399a6afb930a6d1b2a8b52a9c9dd2d0e
emieldepaepe <depaepeemiel@gmail.com>
2014-07-06 23:12:11 +0200

    Added particles

---
+++ Assets/Particles/CayotteEffectsBehavior.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CayotteEffectsBehavior : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartLine()
+	{
+		transform.GetComponentInChildren<DashLineBehaviour>().StartEffect();
+	}
+	
+	public void EndLine()
+	{
+		transform.GetComponentInChildren<DashLineBehaviour>().StartEffect();
+	}
+
+	public void Shout()
+	{
+		transform.FindChild("Shout").GetComponent<ParticleSystem>().Play();
+	}
+}
---
+++ Assets/Particles/DashLineBehaviour.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DashLineBehaviour : MonoBehaviour {
+
+	private float _time = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+		_time = transform.GetComponentInChildren<TrailRenderer>().time;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+	
+	}
+
+	public void StartEffect()
+	{
+		transform.GetComponentInChildren<TrailRenderer>().enabled = true;
+		transform.GetComponentInChildren<TrailRenderer>().time = _time;
+	}
+
+	public void EndEffect()
+	{
+		transform.GetComponentInChildren<TrailRenderer>().enabled = false;
+		transform.GetComponentInChildren<TrailRenderer>().time = 0;
+	}
+}
