8550bfeeb051f4abfef410bceb2120af45e6650f
emieldepaepe <depaepeemiel@gmail.com>
2014-07-06 20:31:15 +0200

    Added pinata damage.

--- Assets/Scripts/PinataEffects.cs
+++ Assets/Scripts/PinataEffects.cs
@@ -8,6 +8,14 @@ public class PinataEffects : MonoBehaviour
 
 	private float _timer =0;
 
+	public Mesh Full = null;
+	public Mesh Damaged = null;
+	public Mesh NearDestroyed = null;
+
+	private Mesh _doll = null;
+
+	private int _damageState = 2;
+
 	// Use this for initialization
 	void Start () 
 	{
@@ -21,6 +29,13 @@ public class PinataEffects : MonoBehaviour
 		_particles.Stop();
 
 		_animator = transform.GetComponent<Animator>();
+
+		_doll =  transform.FindChild("Pinata_Root").
+			FindChild("upperRope").
+			FindChild("middleRope").
+			FindChild("lowerRope").
+			FindChild("pinata").
+			FindChild("Doll").GetComponent<MeshFilter>().mesh;
 	}
 	
 	// Update is called once per frame
@@ -41,5 +56,17 @@ public class PinataEffects : MonoBehaviour
 		_particles.Play();
 		_animator.SetBool("Shake",true);
 		_timer+=Time.deltaTime;
+
+		--_damageState;
+
+		switch (_damageState)
+		{
+		case 0:	
+			_doll = NearDestroyed;
+			break;
+		case 1:
+			_doll = Damaged;
+			break;
+		}
 	}
 }
