6e93e4e3ff9bd28e8ef43a039f89fa46832ad66c
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2014-07-06 19:07:57 +0100

    Scorebar basics & MAC controller support.

--- Assets/Scripts/Managers/ScoreManager.cs
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -69,15 +69,15 @@ public class ScoreManager : MonoBehaviour
     void CalculateBar()
     {
         //Difference
-        float diff = ShepherdScore - SheepScore;
-        diff /= 200; //100 points of difference is a full bar
-        if (diff > 0.5f) diff = 0.5f;
-        if (diff < -0.5f) diff = -0.5f;
+        //float diff = ShepherdScore - SheepScore;
+        //diff /= 200; //100 points of difference is a full bar
+        //if (diff > 0.5f) diff = 0.5f;
+        //if (diff < -0.5f) diff = -0.5f;
 
-        transform.localScale = new Vector3(diff, 0.1f, 1.0f);
-        transform.position = new Vector3(0.5f - diff / 2.0f, 0.94f, 0.0f);
+        //transform.localScale = new Vector3(diff, 0.1f, 1.0f);
+        //transform.position = new Vector3(0.5f - diff / 2.0f, 0.94f, 0.0f);
 
-        if (diff > 0.0f) guiTexture.color = new Color(0.44f, 0.039f, 0.039f);
-        else             guiTexture.color = new Color(0.04f, 0.325f, 1.0f);
+        //if (diff > 0.0f) guiTexture.color = new Color(0.44f, 0.039f, 0.039f);
+        //else             guiTexture.color = new Color(0.04f, 0.325f, 1.0f);
     }
 }
