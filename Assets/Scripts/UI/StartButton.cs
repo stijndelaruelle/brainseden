249cd09d8f3108c9e520dc45fc555418650092b2
Stijn Delaruelle <stijn.delaruelle@gmail.com>
2014-07-07 01:46:18 +0100

    MAIN MENU BITCHES

---
+++ Assets/Scripts/UI/StartButton.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartButton : MonoBehaviour, IButton
+{
+    public void OnMouseDown()
+    {
+        Application.LoadLevel(1);
+    }
+
+    public void OnMouseEnter()
+    {
+        transform.localScale = new Vector3(1.1f, 1.1f, 1.1f);
+        transform.Find("Button_Highlight").GetComponent<SpriteRenderer>().enabled = true;
+    }
+
+    public void OnMouseExit()
+    {
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        transform.Find("Button_Highlight").GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
