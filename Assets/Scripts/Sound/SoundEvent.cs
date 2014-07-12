5e77177172ac13815af423154a2ce3b0aa21aade
brechtlec <brecht.lecluyse@student.howest.be>
2014-07-12 16:13:27 +0200

    Added custom play sound at point to allow for min distance control

--- Assets/Scripts/Sound/SoundEvent.cs
+++ Assets/Scripts/Sound/SoundEvent.cs
@@ -14,13 +14,27 @@ public class SoundEvent : MonoBehaviour
 	}
 	
 	// Update is called once per frame
-    public void PlaySound(GameObject posObject = null, float volume = 4.0f)
+    public void PlaySound(GameObject posObject = null, float volume = 4.0f, float minDistance = 4.5f)
 	{
 	    int amountOfSounds = m_Sounds.Count;
 	    if (amountOfSounds > 0)
 	    {
 	        m_CurrentID = (m_CurrentID + 1) % amountOfSounds;
-            AudioSource.PlayClipAtPoint(m_Sounds[m_CurrentID], posObject != null ? posObject.transform.position : Vector3.zero, volume);
-	    }
+            //AudioSource.PlayClipAtPoint(m_Sounds[m_CurrentID], posObject != null ? posObject.transform.position : Vector3.zero, volume);
+			AudioSource audioSource = PlayClipAtPointCustom(m_Sounds[m_CurrentID],posObject != null ? posObject.transform.position : Vector3.zero);
+			audioSource.volume = volume;
+			audioSource.minDistance = minDistance;
+		}
+	}
+
+	private AudioSource PlayClipAtPointCustom(AudioClip clip,Vector3 position)
+	{
+		GameObject tempAS = new GameObject("PlayClipAtPointAudioSource"); // create the temp object
+		tempAS.transform.position = position; // set its position
+		AudioSource audioSource = tempAS.AddComponent<AudioSource>(); // add an audio source
+		audioSource.clip = clip; // define the clip
+		audioSource.Play(); // start the sound
+		Destroy(tempAS, clip.length); // destroy object after clip duration
+		return audioSource; // return the AudioSource reference
 	}
 }
